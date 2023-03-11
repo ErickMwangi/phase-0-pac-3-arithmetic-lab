06e0cfdb6b31bdb9d03fa9011462f66b40581cd4
ErickMwangi <rickthetri20@gmail.com>
2023-03-11 06:21:32 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) {
+expect (add).toExist
+}
+
+function subtract(a,b) {
+    expect(subtract).toExist
+  }
+   
+  function multiply(a,b) {
+    expect(multiply).toExist
+  }
+function divide(a,b) {
+    expect(divide).toExist
+  }
+  function add(a,b) {
+    return(a + b)
+  }
+  function subtract(a,b) {
+    return(a - b)
+  }
+  function multiply(a,b) {
+    return(a * b)
+  }
+  function divide(a,b) {
+    return(a / b)
+  }
+  function increment(n) {
+    return(n + 1)
+  }
+  function decrement(n) {
+    return (n - 1)
+  }
+  function makeInt(a) {
+    return (a)
+  }
+  function makeInt(n) {
+    return parseInt(n,10)
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n) 
+  }
